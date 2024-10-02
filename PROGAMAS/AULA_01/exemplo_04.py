dcb3339c85f573f8e057ac6b4e2fee391b4157f1
adriana953 <tinamoreira.cg@gmail.com>
2024-10-02 18:18:27 -0300

    Add files via upload

--- PROGAMAS/AULA_01/exemplo_04.py
+++ PROGAMAS/AULA_01/exemplo_04.py
@@ -1,6 +1,16 @@
 # Calcular a idade de uma pessoa a partir do ano de nascimento
-nome = input("Informe o nome da pessoa: ")
-ano_nascimento = int(input("Informe o ano de nascimento: "))
-ano_atual = int(input("Informe o ano atual: "))
-idade = ano_atual - ano_nascimento
-print(f"{nome} tem {idade} anos.")
+def calcular_peso_ideal(altura, sexo):
+    if sexo.lower() == 'homem':
+        peso_ideal = (72.7 * altura) - 58
+    elif sexo.lower() == 'mulher':
+        peso_ideal = (62.1 * altura) - 44.7
+    else:
+        return "Sexo inválido. Por favor, insira 'homem' ou 'mulher'."
+    
+    return round(peso_ideal, 2)
+
+altura = float(input("Digite a altura (em metros): "))
+sexo = input("Digite o sexo (homem ou mulher): ")
+
+peso_ideal = calcular_peso_ideal(altura, sexo)
+print(f"O peso ideal para uma pessoa de {altura}m e sexo {sexo} é: {peso_ideal} kg.")
