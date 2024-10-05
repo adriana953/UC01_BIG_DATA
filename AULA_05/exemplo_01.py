147f624edfac9b8740c8302b1d9e2a29b9af12d4
adriana953 <tinamoreira.cg@gmail.com>
2024-10-04 21:31:39 -0300

    Add files via upload

---
+++ AULA_05/exemplo_01.py
@@ -0,0 +1,8 @@
+print("Usando o comando For.......")
+for i in range(5):
+    print(i)
+print("Usando o comando While.......")
+i = i
+while i <= 5: #Loop Finito de 1 a 5
+        print(1)
+        i += 1
---
+++ AULA_05/exemplo_02.py
@@ -0,0 +1,5 @@
+resp = "S"
+while resp == "S":
+    nome = input("INforme o nome do usuários")
+    resp = input("Deseja Continuar(S/N)?")
+print(nome)
