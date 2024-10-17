a4c02e9d21488ac70561870c383d27b2c88b4f34
adriana953 <tinamoreira.cg@gmail.com>
2024-10-17 18:21:37 -0300

    Add files via upload

---
+++ exercicio_02_04.py
@@ -0,0 +1,10 @@
+import pandas as pd
+temp_max = pd.Series([30,31,38,32,25,22,30])
+temp_min = pd.Series([25,24,26,19,18,16,22])
+media_max = temp_max.mean()
+media_min = temp_min.mean()
+print("Amplitude Térmica em Grau ºC")
+print(temp_max - temp_min)
+print("----------------------------")
+print(f"A média da temperatura máxima foi {media_max:.1f}ºC")
+print(f"A média da temperatura mínima foi {media_min:.1f}ºC")
